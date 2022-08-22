99cba3e4566e998931fe84f65ec9e0ef1606e8c3
thanh-px <pxthanh98@gmail.com>
2022-08-22 09:55:59 +0700

    Update data.yaml

--- preprocess_data.py
+++ preprocess_data.py
@@ -11,24 +11,25 @@ NAMES = ["Human", "Head"]
 def preprocess(task, folder):
     frames = os.listdir(os.path.join(ROOT_DIR, "dataset", "frames", task))
     annotations = os.listdir(os.path.join(ROOT_DIR, "dataset", "annotations", task))
-    for frame in tqdm(frames):
-        ori_frame = os.path.join(ROOT_DIR, "dataset", "frames", task, frame)
-        dst_frame = os.path.join(OUTPUT_DIR, "images", folder, frame)
-        shutil.copyfile(ori_frame, dst_frame)
-    for annotation in tqdm(annotations):
-        ori_label = os.path.join(ROOT_DIR, "dataset", "annotations", task, annotation)
-        dst_label = os.path.join(OUTPUT_DIR, "labels", folder, annotation)
-        shutil.copyfile(ori_label, dst_label)
+    # for frame in tqdm(frames):
+    #     ori_frame = os.path.join(ROOT_DIR, "dataset", "frames", task, frame)
+    #     dst_frame = os.path.join(OUTPUT_DIR, "images", folder, frame)
+    #     shutil.copyfile(ori_frame, dst_frame)
+    # for annotation in tqdm(annotations):
+    #     ori_label = os.path.join(ROOT_DIR, "dataset", "annotations", task, annotation)
+    #     dst_label = os.path.join(OUTPUT_DIR, "labels", folder, annotation)
+    #     shutil.copyfile(ori_label, dst_label)
 
 def create_config(file_name):
     nc = len(NAMES)
-    names = " ".join(NAMES)
+    names = [f"\"{name}\"" for name in NAMES]
+    names = ", ".join(names)
     with open(file_name, "w") as f:
         f.write(f"path: {OUTPUT_DIR}\n")
         f.write("train: images/train\n")
         f.write("val: images/val\n")
         f.write(f"nc: {nc}\n")
-        f.write(f"names: {names}")
+        f.write(f"names: [{names}]")
 
 
 if __name__ == "__main__":
